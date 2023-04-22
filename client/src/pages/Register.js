c4a164bb9d6549e241356215f8b666474501fb37
grmbyrn <graemebyrne_89@hotmail.com>
2023-04-22 21:21:24 +0200

    add context and reducer to handle global state

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,17 +1,18 @@
 import { useState, useEffect } from "react"
 import { Logo, FormRow, Alert } from "../components"
 import Wrapper from "../assets/wrappers/RegisterPage"
+import { useAppContext } from "../context/appContext"
 
 const initialState = {
   name: '',
   email: '',
   password: '',
-  isMember: true,
-  showAlert: false
+  isMember: true
 }
 
 const Register = () => {
   const [values, setValues] = useState(initialState)
+  const {isLoading, showAlert} = useAppContext()
 
   const toggleMember = () => {
     setValues({...values, isMember: !values.isMember})
@@ -31,7 +32,7 @@ const Register = () => {
       <form className="form" onSubmit={onSubmit}>
         <Logo />
         <h3>{values.isMember ? 'Login' : 'Register'}</h3>
-        {values.showAlert && <Alert />}
+        {showAlert && <Alert />}
         {/* name input */}
         {!values.isMember && (
           <FormRow
