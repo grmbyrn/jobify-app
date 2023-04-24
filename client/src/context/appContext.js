5f61834cc15306c5d84fc501013d68f26b859108
grmbyrn <graemebyrne_89@hotmail.com>
2023-04-25 00:53:10 +0200

    add login functionality

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,17 +1,24 @@
-import React, { useReducer, useContext, REGISTER_USER_BEGIN, REGISTER_USER_SUCCESS, REGISTER_USER_ERROR } from "react";
+import React, { useReducer, useContext } from "react";
+import {SETUP_USER_BEGIN, SETUP_USER_ERROR, SETUP_USER_SUCCESS} from './actions'
 
 import reducer from "./reducer";
+import axios from 'axios'
 
 import {DISPLAY_ALERT, CLEAR_ALERT} from './actions'
+
+const token = localStorage.getItem('token')
+const user = localStorage.getItem('user')
+const userLocation = localStorage.getItem('location')
 
 const initialState = {
     isLoading: false,
     showAlert: false,
     alertText: '',
     alertType: '',
-    user: null,
-    token: null,
-    userLocation: ''
+    user: user ? JSON.parse(user) : null,
+    token: token,
+    userLocation: userLocation || '',
+    jobLocation: userLocation || ''
 }
 
 const AppContext = React.createContext()
@@ -30,11 +37,38 @@ const AppProvider = ({children}) => {
         }, 3000);
     }
 
-    const registerUser = async (currentUser) => {
-        console.log(currentUser)
+    const addUserToLocalStorage = ({user, token, location}) => {
+        localStorage.setItem('user', JSON.stringify(user))
+        localStorage.setItem('token', token)
+        localStorage.setItem('location', location)
     }
 
-    return <AppContext.Provider value={{...state, displayAlert, registerUser}}>
+    const removeUserFromLocalStorage = () => {
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+        localStorage.removeItem('location')
+    }
+
+    const setupUser = async ({currentUser, endPoint, alertText}) => {
+        dispatch({type: SETUP_USER_BEGIN})
+        try{
+            const {data} = await axios.post(`/api/v1/auth/${endPoint}`, currentUser)
+            
+            const {user, token, location} = data
+            dispatch({
+                type: SETUP_USER_SUCCESS,
+                payload: {user, token, location, alertText}
+            })
+            addUserToLocalStorage({user, token, location})
+        } catch (error){
+            dispatch({
+                type: SETUP_USER_ERROR,
+                payload: {msg: error.response.data.msg}
+            })
+        }
+        clearAlert()
+    }
+    return <AppContext.Provider value={{...state, displayAlert, setupUser}}>
         {children}
     </AppContext.Provider>
 }
