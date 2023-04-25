b5e5f9f1c954cb4b983f7bf725cafe1dcb4923c4
grmbyrn <graemebyrne_89@hotmail.com>
2023-04-25 12:58:06 +0200

    add sidebar functionality at big and small screens

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,8 +1,23 @@
+import { useAppContext } from "../context/appContext"
+import NavLinks from "./NavLinks"
+import Logo from "./Logo"
 import Wrapper from "../assets/wrappers/BigSidebar"
 
 const BigSidebar = () => {
+  const {showSidebar} = useAppContext()
   return (
-    <Wrapper>BigSidebar</Wrapper>
+    <Wrapper>
+      <div
+        className={showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'}
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
   )
 }
 export default BigSidebar
